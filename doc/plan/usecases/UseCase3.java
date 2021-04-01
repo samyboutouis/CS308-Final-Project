92effcea3a920bd3b3cbae3ef5d543ec0eeaef86
Felix Jiang <felix.jiang@duke.edu>
2021-04-01 15:21:44 -0400

    use case 3 compiles

--- doc/plan/usecases/UseCase3.java
+++ doc/plan/usecases/UseCase3.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b6bafb51abb1ca0c6a3852109055b20a377c32b9285501dfa24a018c6a2bff0
-size 437
+oid sha256:adbbce7d9c9ad0f0218758d0112ef6f1b05677640ebc5fca712f233f3f1db308
+size 503
