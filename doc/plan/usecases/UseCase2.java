42b109d6ba8b60ff73c68943a10d0ba460c125ee
Felix Jiang <felix.jiang@duke.edu>
2021-04-01 15:21:03 -0400

    use case 2 compiles

--- doc/plan/usecases/UseCase2.java
+++ doc/plan/usecases/UseCase2.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d42ca3e17b6bf6f9d442c2151ed0718fc012f3125588cce0f02ef24f3f0f6b30
-size 421
+oid sha256:b90c980ce95842fd95ba36ee49eb210472886eb6c563657a53ee0bfa25cd338d
+size 486
