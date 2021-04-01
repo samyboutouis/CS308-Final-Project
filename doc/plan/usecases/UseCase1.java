cf1b4549ecc531d35d09128a4269903fd070767d
Felix Jiang <felix.jiang@duke.edu>
2021-04-01 15:23:33 -0400

    refactor api structure

--- doc/plan/usecases/UseCase1.java
+++ doc/plan/usecases/UseCase1.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:634ca1ff60b5fd115729afd3626121174825d759c6c587942a8efba6d39c66d0
-size 871
+oid sha256:ca0a2a1b5b401416e26e7dafac955572e91a900b388c7d02bdb7efadec1acedc
+size 880
--- doc/plan/usecases/UseCase4.java
+++ doc/plan/usecases/UseCase4.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b60d74c2e6999bc0fd7a5c9d4dc19e516c9a4e4ab2171ca589f7c2100ff5b787
-size 533
+oid sha256:6238b4e912fe522e2f82755eff9dc0f2c3a6519208426e3ebff1701d91752f90
+size 562
