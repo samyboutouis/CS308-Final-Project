2c86d68bbfeccc1190924ea3465928d37220c228
Donghan Park <donghan.park@duke.edu>
2021-04-04 01:48:52 -0400

    added temporary screen manager system

--- src/ooga/player/SplashScreen.java
+++ src/ooga/player/SplashScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12cd7e677faeee336c7b7b2d27f0930e18e824c4aad1b1970a965062698dda2a
-size 352
+oid sha256:ae6864bea399de95c74a3fc2c5dfdb0e9ca7e3180c6170d7dac7713d7f5b71ff
+size 807
