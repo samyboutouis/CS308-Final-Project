79ce1c6549fc82e75d428096a58d7d4f4bcab3b3
Felix Jiang <felix.jiang@duke.edu>
2021-04-04 21:58:24 -0400

    create interface for parser

--- src/ooga/data/Parser.java
+++ src/ooga/data/Parser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a97462fbb0d732fa5d3cbf38ecfb87f7bbbe4c40a12c690cc19c4e822baeca05
-size 46
+oid sha256:dfcbd9fec3c086bb210b3d8e3933c3e8478a72a3bfb2889905111fe409437cd8
+size 81
