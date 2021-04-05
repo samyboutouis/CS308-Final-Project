dfabc09fe0ef83331e377564db6c884221dd0e44
Felix Jiang <felix.jiang@duke.edu>
2021-04-05 01:24:41 -0400

    more logic goes into level parser

--- src/ooga/data/backendMainTemp.java
+++ src/ooga/data/backendMainTemp.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bc407680d4aaed66311ca524bedb04801d3b931ca667b2409b55dcf6db22a64
-size 437
+oid sha256:716497263a98555332754b04072710c5047f863ba7be376ad7177d9cc3f1e6ba
+size 340
