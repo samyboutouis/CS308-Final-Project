c0865f427169ae52455f71722ea33cea5c874be6
Felix Jiang <felix.jiang@duke.edu>
2021-04-05 01:11:09 -0400

    level parser imported level

--- src/ooga/data/LevelParser.java
+++ src/ooga/data/LevelParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ff9cd19115247bf5281a6ff1e4a1f0f2db913b12b583162aea89f8f0b98bbb
-size 150
+oid sha256:c83a16b8d0e0b68794883f8c1556a9cf519b8ad983483bab3af1669ce0fb3329
+size 175
