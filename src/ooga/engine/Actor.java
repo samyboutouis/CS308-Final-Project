21ab40697c0f06207670205b3fc6d7721cbcbcd6
Felix Jiang <felix.jiang@duke.edu>
2021-04-05 00:24:51 -0400

    actor has a position

--- src/ooga/engine/Actor.java
+++ src/ooga/engine/Actor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c43cfc0e9fac07732c0bf8b100f725d91eab8c42b72b8cc81fbc3eed908d9be
-size 197
+oid sha256:6526662afaf70d543b52e472cd4c89ad519b8ace4c7c0ae7e24855eed0d59afd
+size 249
