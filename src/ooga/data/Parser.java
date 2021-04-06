5da16a3d139e20d110a19c299b7d02c4d19f6b99
Felix Jiang <felix.jiang@duke.edu>
2021-04-06 03:35:07 -0400

    parser throws all the reflection methods

--- src/ooga/data/Parser.java
+++ src/ooga/data/Parser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4277c43849270e82ec72200aded513d451766800fb3976a66fb965097f1d6821
-size 458
+oid sha256:e7ae1e24b9f5c5a10b065dffe05a9aa54f089080e696bb848ee6f60e414e1d99
+size 860
