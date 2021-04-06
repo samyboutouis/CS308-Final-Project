20472aa27609cd8dfbd161220e1073deab1fbe1c
Felix Jiang <felix.jiang@duke.edu>
2021-04-06 03:18:36 -0400

    level implementation takes in level and viewable level

--- src/ooga/engine/LevelImplementation.java
+++ src/ooga/engine/LevelImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32055fc386bf08f909455578e3baf7395845f6104a7bb8ab69fc30a037d410bf
-size 273
+oid sha256:e73bace59f114b8101f5d7b73a31c3cbcc44c531390b200085bb82a010e84d46
+size 663
