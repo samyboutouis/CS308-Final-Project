2457eda28d354a613138dfd8a2843afe9c223522
Samy Boutouis <sb590@duke.edu>
2021-04-06 00:30:12 -0400

    Delete instance variable for factory;

--- src/ooga/player/controllers/SplashScreenController.java
+++ src/ooga/player/controllers/SplashScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bbcb05c6afaa2e9478f45a5daea3382c0af3c65968ea1335ed7ba84fb7ccdee
-size 597
+oid sha256:b35605d5c51a572bb8cd3dcfea604d79c566bab689a97ad453a8e01ae53fccde
+size 564
