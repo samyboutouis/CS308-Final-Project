8a047f5831d7aa1f56bcc435a1141187b41e8659
Samy Boutouis <sb590@duke.edu>
2021-04-06 00:34:00 -0400

    Add play button

--- src/ooga/player/screens/SplashScreen.java
+++ src/ooga/player/screens/SplashScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a0778fe4825062e5405cea5e496928cfc36a166976be7a98311ff53f9a4390d
-size 1152
+oid sha256:2ca5b9ec66f8fd97dc73705c77854d4caff848e3e68148235a6d7f86c014e442
+size 1148
