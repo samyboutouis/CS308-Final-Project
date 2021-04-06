177082ad7ebb66e074114304c62c0a5d9c9041dc
Samy Boutouis <sb590@duke.edu>
2021-04-06 01:52:48 -0400

    Make play level button

--- src/ooga/player/controllers/GameSelectController.java
+++ src/ooga/player/controllers/GameSelectController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e74f40febc311f59901d6a3f1aa017b51bae3fe7b97da1c5e1841c97d5bb64c7
-size 491
+oid sha256:c0c8f9444b2fa03c028e29b4e38616e5b27c28c5ab2a29b977a2764d1e76e39e
+size 572
