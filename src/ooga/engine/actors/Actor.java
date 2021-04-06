9ba7b1836568a62d2a4b688b25cf1748d7643a59
Felix Jiang <felix.jiang@duke.edu>
2021-04-06 03:24:28 -0400

    actor doesn't need to know position on creation

--- src/ooga/engine/actors/Actor.java
+++ src/ooga/engine/actors/Actor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:143f828de595cad527b933dcfafc241e39b6b9b47375ce86a2628a26850e6d94
-size 2129
+oid sha256:c4c08b3c71a901c8e43acc63a3f77c34632bf4f4457834d2bf0acc855232b623
+size 2373
