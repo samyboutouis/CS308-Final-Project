e906432855cbbba8c6ba0fbc13d4d66a0810ae68
Felix Jiang <felix.jiang@duke.edu>
2021-04-06 02:40:57 -0400

    get dimensions from grid

--- src/ooga/data/Grid.java
+++ src/ooga/data/Grid.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6b40d86e6d83646cfc8afd20f86537d5164410b7b8adcf23d6b0826abf3f36e
-size 578
+oid sha256:0ef9a884249e50c3755e6990e4dea113cb775b54cdfb58e8b29e41df5657037f
+size 704
