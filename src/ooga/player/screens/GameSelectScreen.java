9581270a0a93c6e1b9639a0e47f254011e6e85c9
Donghan Park <donghan.park@duke.edu>
2021-04-06 21:19:39 -0400

    added level descriptions to level tags in game select screen

--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee4f4d61c9df12ad991d723413fb7e0ac75f7d1eae6eb9ddf3c9ef2800392217
-size 3209
+oid sha256:1a99167a32239743d101332f06b90dd4e4217b38efe5cfcaf7ccdf0d2dd44788
+size 3843
