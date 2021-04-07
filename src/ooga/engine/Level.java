5fcd3559095256ba6f40a0c1f020a01105e39632
Felix Jiang <felix.jiang@duke.edu>
2021-04-06 20:56:55 -0400

    level extends viewable level so controller has access to level, while front end has access to viewable level

--- src/ooga/engine/Level.java
+++ src/ooga/engine/Level.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd161183c4ed0e543a352563812b8e9c896f64de6d25b368dc7092959b8ac72b
-size 151
+oid sha256:3ec2331891c20b83913c283b772d7b62f0ad240d08f7ac3d0d1c832dd5a37e79
+size 172
