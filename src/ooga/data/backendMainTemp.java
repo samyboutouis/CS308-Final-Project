38af960932be7cefd0290fa25e307fbaeb1e0679
Donghan Park <donghan.park@duke.edu>
2021-04-06 23:12:58 -0400

    added styling to game select screen

--- src/ooga/data/backendMainTemp.java
+++ src/ooga/data/backendMainTemp.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc79ce534fdfcd6225680c54bb938d6186be89bcd152c5226d18b6d3bed3a6be
+oid sha256:e3515eab832b23c8b7fddd9fbf201340d2fcee4433f04adde833c7d6aa127de6
 size 899
