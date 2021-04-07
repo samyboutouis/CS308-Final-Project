334df62a2fa6d279194118ebf6d9383c4eb0c126
Samy Boutouis <sb590@duke.edu>
2021-04-06 21:46:18 -0400

    Fix actors on screen

--- src/ooga/player/controllers/GameScreenController.java
+++ src/ooga/player/controllers/GameScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9fedb99ca7a10f30a557710465e4e23f61579fd04000b76a065faac81549075
-size 2090
+oid sha256:be268c30fcd3c6bc1327ff102ba4538ddeec8bd24578f3679362830245eb05f5
+size 1890
