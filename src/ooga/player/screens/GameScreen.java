c2c3a38677b9a59b567f7f846be467d5c6e70798
Samy Boutouis <sb590@duke.edu>
2021-04-06 23:56:39 -0400

    Create HUD display

--- src/ooga/player/screens/GameScreen.java
+++ src/ooga/player/screens/GameScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb6eb7c3635df93f6f40ab05da82fa66f7f39b907d03cd4c878deeaac188c4f9
-size 3399
+oid sha256:506a1c3a0e2575ab52310e7d7596ce8920a09653e17749273ae66dec9823e214
+size 3578
---
+++ src/ooga/player/screens/HeadsUpDisplay.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cb67760f661099a0cf9f243406e5870621501e56da9fa24a86d33f6c473ad03
+size 1728
