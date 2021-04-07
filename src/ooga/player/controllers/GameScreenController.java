fa9a1966f1282835da621c6129fb17df82ea44fe
Samy Boutouis <sb590@duke.edu>
2021-04-06 23:11:48 -0400

    Create timeline'

--- src/ooga/player/controllers/GameScreenController.java
+++ src/ooga/player/controllers/GameScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be268c30fcd3c6bc1327ff102ba4538ddeec8bd24578f3679362830245eb05f5
-size 1890
+oid sha256:de36c54bbe1507e440592e8593acffd1aff3e54839a4e6d33286bbb4b3e9c368
+size 2736
