10ec801445b2d0170bee6054df5cf19a6244163a
Donghan Park <donghan.park@duke.edu>
2021-04-06 21:09:25 -0400

    added method to send level file to backend from game select screen

--- src/ooga/player/controllers/SplashScreenController.java
+++ src/ooga/player/controllers/SplashScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8a2fe71bb6030a514ca7c5376f16be663717672e4fea26141429bb3819196f9
-size 578
+oid sha256:114afba489a11cceecde84c21584941b37592c118f6cfb4122b941833766be22
+size 590
--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:385cbab4b3fd3e8a97389567416abf5318ec25db073c9ff826402c0e12a26a5d
-size 2558
+oid sha256:ee4f4d61c9df12ad991d723413fb7e0ac75f7d1eae6eb9ddf3c9ef2800392217
+size 3209
