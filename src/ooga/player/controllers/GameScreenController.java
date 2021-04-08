4c9d27b615971283ded92de0ced17e8e554445f5
Felix Jiang <felix.jiang@duke.edu>
2021-04-08 00:33:48 -0400

    add collision triggers to level

--- src/ooga/player/controllers/GameScreenController.java
+++ src/ooga/player/controllers/GameScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ad36ab0feb9ef0546b2763ad814876ba77d49e0c4393aa9d00302040c330712
-size 5227
+oid sha256:3af1ac58253de9dba7b9db11f222e657eb2b6d167617a16d62ace0ab15c8c9e2
+size 5578
