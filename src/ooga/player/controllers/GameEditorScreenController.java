a37f5500a2ddd07cc42b216cf78b98303cbabb65
Donghan Park <donghan.park@duke.edu>
2021-04-08 01:09:56 -0400

    added package to store all json files for game objects

---
+++ src/ooga/player/controllers/GameEditorScreenController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85894a902005c2ec972ae4bb372425079dd81e7a81f191a4848a8958a2db7b4b
+size 306
---
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:82c60d5be59ead425c07fd3318d0a8600dbeb9effcad4ce8563265befad7746a
+size 946
