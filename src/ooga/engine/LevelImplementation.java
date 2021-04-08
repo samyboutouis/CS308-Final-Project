7089afbb637b05970c10c895297f07c3630a63d6
Felix Jiang <felix.jiang@duke.edu>
2021-04-07 23:26:39 -0400

    comment about collision checking

--- src/ooga/engine/LevelImplementation.java
+++ src/ooga/engine/LevelImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f30d5221793715d6fc782f3ab26efedfae88115ccedab779d5ef479b49e4a86
-size 2639
+oid sha256:c575f9ff12c293e047d122f4039490484a586e5bb18d0ceb64f8921e1b7de44e
+size 2936
