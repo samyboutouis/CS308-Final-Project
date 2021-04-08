50f810b7646829b18e07bcf170f3323c75f52f78
Felix Jiang <felix.jiang@duke.edu>
2021-04-08 00:33:15 -0400

    change jump velocity

--- src/ooga/engine/actions/Jump.java
+++ src/ooga/engine/actions/Jump.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be25a18dcef42a2875a12db08d0d65df9750d0ae6bae2bb6037f609fa3ca211c
-size 417
+oid sha256:657dd9452090e565d1b95fc27639e25185ca653fe6a9a1510fad0aa53a6838d4
+size 416
