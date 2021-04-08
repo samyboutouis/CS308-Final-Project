e00f3121c7bd53b1af80bd097c50b8142cd896b6
Felix Jiang <felix.jiang@duke.edu>
2021-04-08 00:33:32 -0400

    make sure to call checkCollisionTriggers

--- src/ooga/engine/LevelImplementation.java
+++ src/ooga/engine/LevelImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b09f600e8ba873ff2d6dd760baa2e4f05666f1c2893cbdf61a5d37d73c8cf0e
-size 3593
+oid sha256:c89f90cb12becc3623693b6716ec6172802205591dce8f1bb81dcbf9e5ddce6d
+size 3592
