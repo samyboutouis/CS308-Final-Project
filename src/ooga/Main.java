6eaece0cc69eb2ffc237706438e6209c52fe0b42
Samy Boutouis <sb590@duke.edu>
2021-04-07 23:34:47 -0400

    Modify stage and add tests

--- src/ooga/Main.java
+++ src/ooga/Main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a50e9b1d45e5b4a2aca2e80fd8b4bf91715b0cecfbc41a62fe62eea900522a7
-size 1466
+oid sha256:5c82f78cd9dccc94595cb4457a7bdec0646bf0be1ab7d72b6ff22fae7ddf5e63
+size 1451
--- test/ooga/player/SplashScreenTest.java
+++ test/ooga/player/SplashScreenTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:735f952678ee8194b3b8c9027bfc948a3edc3b86419bb4dee8e9507a647f2eca
-size 761
+oid sha256:fa0ace0bcb9e0829c333067b44844ec30f8f15d5297338f5da44379e0c0bfe3c
+size 879
