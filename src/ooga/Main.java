2cd479cc9c5e12c063b31c98959af5232d974f75
Bill Guo <wg78@duke.edu>
2021-04-08 02:10:35 -0400

    added camera

--- src/ooga/Main.java
+++ src/ooga/Main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a91a21572eb6665d7398acdbeb9d6994f91acbd75e52889245a69d1c8b1db6aa
-size 1520
+oid sha256:143df1992fd5e3a9494074d939bd624e3e90aadc55f58455c3390a01f645ec0b
+size 1518
--- src/ooga/player/controllers/GameSelectController.java
+++ src/ooga/player/controllers/GameSelectController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e2d29f079e34cb810bc85f2402bc2a028ca8afdfaf6c1aba571240e95a50d57
+oid sha256:07ec47cf5bfab6b80143a3d0fdcfe1db1857b243face6d51ceabdde34626aafa
 size 1349
