b3018c9cab5f04c53ddab23be3845b170a469523
Felix Jiang <felix.jiang@duke.edu>
2021-04-08 00:34:04 -0400

    avoid checking collision with yourself

--- src/ooga/engine/triggers/CollisionTrigger.java
+++ src/ooga/engine/triggers/CollisionTrigger.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a43e8e58ed387c3418ea16c732faf9f14d0d2577813504c9b2451610c11d1da
-size 1659
+oid sha256:dbd12662ccb36562827f197afd94bd02146678fdcad8d902b2e67b5a10ea1c38
+size 1722
