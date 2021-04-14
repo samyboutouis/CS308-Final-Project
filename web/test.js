6a1181fe65e76c1a26ed3260aa0b9d5c69c67b8c
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 16:30:01 -0400

    find a document in database

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd84d2593556b826c0189276c8ca506a9f74b4d6b97fc3264a4234a629a8748c
-size 2744
+oid sha256:c1583e4c8bf5cf8fb4b6430c0e7e209a3cd4e61c543a102d2490baac942667d0
+size 3021
