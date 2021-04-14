5bc2d6df2ae6714fbe08948779202c9104e81866
Felix Jiang <felix.jiang@duke.edu>
2021-04-13 23:31:30 -0400

    rename manager to methods

--- src/ooga/engine/collisions/CollisionChecker.java
+++ src/ooga/engine/collisions/CollisionChecker.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aabb169c1d57add76f2f572ca10dd1183cd77fe1e143fa2965c5e4f659b00fe
+oid sha256:c10c4ddec6b5cf64856f3affc4cb735903dc56844cd0beeba0b85cfcc88c0245
 size 3074
--- src/ooga/engine/collisions/CollisionMethods.java
+++ src/ooga/engine/collisions/CollisionMethods.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73fcf219ca0c19b6d7b493f8c198c0d5e1c32c0e3995ac0f3287c5f5ccd7b00c
-size 651
+oid sha256:8056f4a3110519eecfa3f893ccd88d24317be8d273c64a19e2e63e6e63b4353a
+size 730
