bee5ae6e6dbd54b70b8dbc21bc3eaa9413f1cbce
Samy Boutouis <sb590@duke.edu>
2021-04-13 23:16:50 -0400

    Add style class to XML parser

--- src/ooga/player/factories/NodeFactory.java
+++ src/ooga/player/factories/NodeFactory.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44f859caa9f03cc3fbdfe7c4fbb8bab182d1efd3861ecb1927bd6d34d8da0e45
-size 3841
+oid sha256:0ce9e7a2a62ce776371e9d85459f1753a963fb34c8c04076af3ea394be7ffdea
+size 3973
--- test/ooga/player/SplashScreenTest.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:420b89bc6ab7de86562d16b6b9252f17b2c7a3605323b2a38a3abe15966ab518
-size 1063
