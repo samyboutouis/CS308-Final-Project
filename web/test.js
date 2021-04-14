3f750b74ac06404926c2ef9563ef94f3f799e516
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 16:05:31 -0400

    list collections

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f55eaf9f4688dba1385c461a1596acfd4d47a3fa0660efec17f4f2fbb0e311db
-size 1978
+oid sha256:148de3dc69ab074e27185f7c48261f91889ce4fdbe62e758b93ad4d1276228bc
+size 2249
