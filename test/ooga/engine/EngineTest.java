97b11ef7156e531e8143087065420cad8e0252ec
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 01:24:45 -0400

    hardcode level constructor for test

--- test/ooga/engine/EngineTest.java
+++ test/ooga/engine/EngineTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b25939a995adaf25efa52844fc4e518a0fff9e48a064b28dd480b9de70bd8d2b
-size 4402
+oid sha256:32bdd3f080e030fb27f66b9d47f635ca8f564431e69c5594bd1904bd258cb4ed
+size 4598
