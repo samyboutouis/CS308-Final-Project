412b6c3e42c6fbc5270b55362f08a15d28302cfe
Donghan Park <donghan.park@duke.edu>
2021-04-13 23:35:40 -0400

    implemented sxml for game select level tags

--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bda03fdf09e353bad435f031ac68c194547f7e042e5db9853e266e75e832f64
-size 4141
+oid sha256:815f79e5fd0141a9f76bb620842ac7a6f57e04f3fe21fb6065792796bfbb8990
+size 4048
