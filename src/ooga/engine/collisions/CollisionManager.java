030eeebc5c81ca0560752d953fdb86be6ba12b76
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 01:33:08 -0400

    manager returns collisions map for tester

--- src/ooga/engine/collisions/CollisionManager.java
+++ src/ooga/engine/collisions/CollisionManager.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58ea8524fcbbc36c517582863e936ae9c140c3e0884f14c4e33a5bb34d5a0353
-size 365
+oid sha256:5c1a5a36afc3a06d599b86696d96b896a73ab67bff435f837665b20a49c077c6
+size 453
--- src/ooga/engine/collisions/ManagerImplementation.java
+++ src/ooga/engine/collisions/ManagerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c9b28d4d04cfea2d3c65bf10a510fb8c477ef51b8511f7d71ab6b541a02a56
-size 3123
+oid sha256:3d965b56b9d2d387423e1f9ea307beed152a583b28a50908be1aab63d4699856
+size 3264
