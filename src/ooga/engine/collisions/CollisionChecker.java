05e569f4b4a551f1d0c04569ffa3e74a6eb64d59
Felix Jiang <felix.jiang@duke.edu>
2021-04-13 22:48:25 -0400

    use a collision detector

--- src/ooga/engine/collisions/CollisionChecker.java
+++ src/ooga/engine/collisions/CollisionChecker.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2deaf940cdc8f43e35905e0e2a5c6dd5b0d4aa50a99a563d04cb7b910ba3d60
-size 3594
+oid sha256:a22390353ba803b8c14bdc3d889d65db8e045ed0a8cacf7e1f707479e86361d3
+size 2896
