10cdd69d130801c52662490b7b2ef022db248b66
Felix Jiang <felix.jiang@duke.edu>
2021-04-13 23:32:23 -0400

    rename checker to manager

--- src/ooga/engine/LevelImplementation.java
+++ src/ooga/engine/LevelImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:583f971598965b653bcffbfc33531342332f28eacb0ee28dae24f0f0f40275b7
-size 4218
+oid sha256:dc8489e28261dfc9db449037a252b71da6ccecec81012a355729ed60783f03a1
+size 4154
--- src/ooga/engine/collisions/CollisionChecker.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c10c4ddec6b5cf64856f3affc4cb735903dc56844cd0beeba0b85cfcc88c0245
-size 3074
