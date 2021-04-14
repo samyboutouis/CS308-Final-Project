53a1550908a1a17f2252c9bd02ab1ee9b01c9144
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 16:35:03 -0400

    find level in db through get request

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4778c59db43b881baf102da98ba647ecf735046774c102a18d3336b811d2c7ed
-size 3118
+oid sha256:843faef58d5f21c4c9f10413e24183df1aa70c785e03caa5cf0796beb02103d7
+size 3157
