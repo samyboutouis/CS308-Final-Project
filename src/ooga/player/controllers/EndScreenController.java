3b9c4d73c5c7a501f800c452e1b0decefce772e8
Samy Boutouis <sb590@duke.edu>
2021-04-13 23:41:16 -0400

    Make end screen boilerplate code

--- src/ooga/player/controllers/EndScreenController.java
+++ src/ooga/player/controllers/EndScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10c7add0f3426bbf1b192c29880e9da2d3cb9040e1c3e25af9c16d72ab2b7258
-size 203
+oid sha256:7d3b0202771d612cecfc5481c73ea53c93daaf73f4809a66ccadac54a368f047
+size 443
