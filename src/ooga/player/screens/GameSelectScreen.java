394fe7d9c17956a74627ab33ecacbcf728afef25
Donghan Park <donghan.park@duke.edu>
2021-04-13 22:39:52 -0400

    fixed sxml bug

--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4075de7c9ec6dfbd0d0689032cd306ce81ae5fa8fc10fd6a52cb28e9a8fc82b
-size 5135
+oid sha256:ef79a77dd708725b3d46674213a99ed84dc75267f4bc221122692d373071567f
+size 5104
