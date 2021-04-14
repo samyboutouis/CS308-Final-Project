ce12cdbd8b3aa998fd5da13c4597be328d79a690
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 15:24:52 -0400

    check if map is null in methods

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd56a93ebe98180dc16bd53d6e58e6619926d9358ee8680d14cd3dd4af18b76
-size 1799
+oid sha256:2eebef3795c2cfee95c18b33f99e80ec14d1d29c02765c994eed0995360be053
+size 1821
