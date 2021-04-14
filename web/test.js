28135e84d3be34a696f471f3b60d76030af3a2ad
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 11:59:20 -0400

    new method to add to database

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d04437f2a002d9b59ddea1d5470c596ccf89c8ce92e8b0a908c7a0d2ea73f26d
-size 1830
+oid sha256:8252e7f2202e51e73d7c9b2cc28df00f461ed0009ebcfdd82c1f525d3ec32f4b
+size 2046
