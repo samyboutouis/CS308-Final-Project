db8625503e2eaf8511c90296072464ec1e19c10e
Felix Jiang <felix.jiang@duke.edu>
2021-04-13 23:47:34 -0400

    only refer to interface in composite

--- src/ooga/engine/collisions/ManagerImplementation.java
+++ src/ooga/engine/collisions/ManagerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f1a556bd6b5091a3c835eeb38cfea72920f2e72340d8aab4c1bf252c2272a9c
-size 3133
+oid sha256:d4db99d201b5a3e6559d26e2e70b3fd175d241175c468d3ee0c0e49bc23afa65
+size 3128
