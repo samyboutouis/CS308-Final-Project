22e37b33afa02fceccc951f831d84288d554765d
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 16:32:58 -0400

    can get level name from parameters

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1583e4c8bf5cf8fb4b6430c0e7e209a3cd4e61c543a102d2490baac942667d0
-size 3021
+oid sha256:4778c59db43b881baf102da98ba647ecf735046774c102a18d3336b811d2c7ed
+size 3118
