8fd68b6acc98588897c6911e93e8ee67082a401f
Felix Jiang <felix.jiang@duke.edu>
2021-04-14 01:25:43 -0400

    collision parser works

--- src/ooga/data/CollisionParser.java
+++ src/ooga/data/CollisionParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:504df6e6e397f9443880d5a233c004c4f036bb62df40bb5c5d553ea4cd7baec0
-size 2613
+oid sha256:12c6c634875e53407d620f9008be22fdf8bf9231fd7eb3c2469249eaeb153435
+size 2620
