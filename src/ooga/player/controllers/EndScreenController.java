c945155026bbd8eddbd0fedf4f58ea82a4799a65
Samy Boutouis <sb590@duke.edu>
2021-04-13 22:19:47 -0400

    Start on winning and losing screen

---
+++ src/ooga/player/controllers/EndScreenController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10c7add0f3426bbf1b192c29880e9da2d3cb9040e1c3e25af9c16d72ab2b7258
+size 203
---
+++ src/ooga/player/screens/EndScreen.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0d3e6b09d7e6951b10aa13c2c7bac03a603192454b6f8757e324bfda1500f87
+size 849
