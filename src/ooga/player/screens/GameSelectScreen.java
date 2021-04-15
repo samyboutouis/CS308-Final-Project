ca1a70a2ac5a1a23004617a4d0dedf33dee7dd0f
Samy Boutouis <sb590@duke.edu>
2021-04-15 01:33:25 -0400

    Set ID so tests work again

--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb2ee71f20225cb440d34b60b3eb12557ed8b90acad46737c4338af8e6e1689d
-size 2957
+oid sha256:234dae2e92fa68f5f6c38db4854e42c149d8e46d7abae02f9a4aa78375d5497e
+size 2992
