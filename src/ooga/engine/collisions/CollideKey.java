52ad9ed78337f5ad32ed6d66c33d17982dd37f30
Felix Jiang <felix.jiang@duke.edu>
2021-04-15 00:59:23 -0400

    map checks hashcode not equals so we need to override hashcode in order to allow custom objects in a hashmap key

--- src/ooga/engine/collisions/CollideKey.java
+++ src/ooga/engine/collisions/CollideKey.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:face07459275795069b4e7895ea65b67e4ef1471e454f7c403c397bd4b36c9d4
-size 939
+oid sha256:97b9ff0a1cf7717a2ccb20d4fc6dcfe84d20ed2f04137bab684ddf24c2549458
+size 1093
