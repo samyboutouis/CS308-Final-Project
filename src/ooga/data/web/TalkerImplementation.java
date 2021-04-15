a16f953b92d9dd9316c8778738c772312c14b7a1
Felix Jiang <felix.jiang@duke.edu>
2021-04-15 11:42:54 -0400

    basic version of post and get works in java code

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28021d001381f598296f287f0a5ba6174b25994aef7f5a3c92e998552a9c8ca9
-size 1328
+oid sha256:77cc5e5de3ab78bd392f38f47b6cc3461421cb169fa854b121d1362a64470db9
+size 2591
