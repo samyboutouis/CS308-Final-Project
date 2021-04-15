008844989ac157bdee66e519c697a31aef4b993d
Bill Guo <wg78@duke.edu>
2021-04-15 00:00:02 -0400

    added coin image

--- src/ooga/player/controllers/GameScreenController.java
+++ src/ooga/player/controllers/GameScreenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf566e531e35e8f4039775094df9dfe722914b0b8bf3d5ed76edf70126b6477b
-size 8105
+oid sha256:134364c9a37daaa73c896b14aa1b38ee08e5a786c46bdc7134be02ee485b9bbc
+size 8004
