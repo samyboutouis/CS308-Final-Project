146fc523a78709e69d061e270ae438dadc6b15b5
Felix Jiang <felix.jiang@duke.edu>
2021-04-15 02:11:06 -0400

    add semicolons to code

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:497bc69e653374007721acc55b7703fb854c92b3c7f48ad996a451c1c4f9a605
-size 5158
+oid sha256:2a2c700ef4bdcdb79291d51225dc7aaee83ca5e8cbf13b0ba0da6cf9d5a1e526
+size 5187
