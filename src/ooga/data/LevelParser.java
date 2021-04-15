c5761c40ea497c9f44a6ded925cd5b9850e5495a
Kevin Yang <kevin.yang@duke.edu>
2021-04-14 21:39:25 -0400

    level parser changes

--- src/ooga/data/LevelParser.java
+++ src/ooga/data/LevelParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10b7861fd3c59ee5bc2c7d5880fb7d38b41d2488e560defd0f985ee302a4305a
-size 3332
+oid sha256:80f1db9d57d0bf6fbd1934040dd8338305db02c9f96313ffff404269aa9ab003
+size 4000
