cdcf83f58b5fa2ec695394ea3ca33d3bfe85b089
Samy Boutouis <sb590@duke.edu>
2021-04-15 14:45:14 -0400

    Styling changes for end screen

--- src/ooga/player/screens/EndScreen.java
+++ src/ooga/player/screens/EndScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d75a2e271f6a85ce83350aa1bbefd562b490cdc7acc83532657000448ab37f17
-size 980
+oid sha256:74e37cfbfded4a2f1f293ba854c7b2d96147f9f19a5a6f50f50f452f43948262
+size 1045
