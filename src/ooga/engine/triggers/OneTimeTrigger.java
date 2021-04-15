2bd704a38904e318b038d212010ef6d676439a6a
Bill Guo <wg78@duke.edu>
2021-04-14 22:37:18 -0400

    added comments to Triggers

---
+++ src/ooga/engine/triggers/OneTimeTrigger.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df27e3dd7e7e904bccb07a9a5618ef2e92f8fbe52142bea50bfdbea775689041
+size 537
--- src/ooga/engine/triggers/Trigger.java
+++ src/ooga/engine/triggers/Trigger.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18062fe131e11316107fc39ce4326b698822461842240b29921f78a3f0d5b2fe
-size 453
+oid sha256:2d46f4bba938aab03c413ff0f73a3f5c7de82aeacc63b424ec2c0da48a06d94d
+size 906
