8a63b50e2d43b66787177383e8aa4c729443db0f
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 18:34:55 -0400

    rename key

--- src/ooga/data/DataParser.java
+++ src/ooga/data/DataParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3a96caee99f08e017cd3841acdc5594f9eaeaafe9a0e0327bb4a2ec98c4a77
-size 1440
+oid sha256:98ac975274d5b7aa068752662d3ae00a3a407f59517ec650b48cb200746bbd35
+size 1439
