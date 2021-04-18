d23435625d3e321a051a6bbd71dc0a297b12d3da
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 17:26:53 -0400

    levels to level

--- src/ooga/data/DataParser.java
+++ src/ooga/data/DataParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10314117d97154de47bcd9efb5cdde7486b2f3fb2b05d02c635de9dac33e52a8
-size 1441
+oid sha256:3d3a96caee99f08e017cd3841acdc5594f9eaeaafe9a0e0327bb4a2ec98c4a77
+size 1440
