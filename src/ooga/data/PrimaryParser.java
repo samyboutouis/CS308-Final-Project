9c04e9189c7469d84f862e31a36799abab5f457c
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 17:27:06 -0400

    can convert json to files

--- src/ooga/data/PrimaryParser.java
+++ src/ooga/data/PrimaryParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9fd2824189ee377ced1aa2ad5a74006463c59db7f7a39fef08602f36fcca1dc
-size 527
+oid sha256:e00bac7ae44a6bc1ded50881427e9690445407ff2dc34574be792fb030ea32a3
+size 1628
