a23f357f1138f885e7744e763e5410df2e5317ec
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 02:39:43 -0400

    implement the interface and make other methods private

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aef85caa3dca334afee72524b09930f9c90b4f42066ba022a55b92417c7cfa1
-size 2152
+oid sha256:97e53e22e84c2f365e1c0277c3a5197c28227afdf8e8dc5cee9b7b00d66abd76
+size 2395
