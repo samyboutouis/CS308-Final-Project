0db3ef6fbf06a9afd84a8bf55b5dff97e4b88f8d
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 18:10:54 -0400

    DELETED properties file creation for actor map

--- src/ooga/data/LevelParser.java
+++ src/ooga/data/LevelParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83adaf8d321be9de033b7864f37260b5c26e055c84c168085787a8afcc41a2e8
-size 4545
+oid sha256:a428cab6487379e07d7db138ed36f7ed421db41b358632dfd82f86dc8ad94edc
+size 4274
