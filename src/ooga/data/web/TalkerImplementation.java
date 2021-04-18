226aaffc5b504f91d7180abec60773260047ba94
Felix Jiang <felix.jiang@duke.edu>
2021-04-18 02:41:17 -0400

    don't hardcode URLs

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e53e22e84c2f365e1c0277c3a5197c28227afdf8e8dc5cee9b7b00d66abd76
-size 2395
+oid sha256:628da6eaa0818ad7367132b3da7306ef644154c57b9847574ff5da04e3c8d51a
+size 2485
