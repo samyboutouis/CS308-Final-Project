a3541bb1a0273c8767fd8bbd35cc968603bc148d
Samy Boutouis <sb590@duke.edu>
2021-04-18 22:59:13 -0400

    Working sign in form that saves user info

--- src/ooga/player/screens/SignInScreen.java
+++ src/ooga/player/screens/SignInScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c10359933f70eac41c7b219ed5235b162328b0f8d29cca3478b2e90745eff107
-size 463
+oid sha256:6e0b4471a3cd2192d5dab6a6d109232660a4fa4f83dc8060ae2ee9ca88c6dbcd
+size 1472
