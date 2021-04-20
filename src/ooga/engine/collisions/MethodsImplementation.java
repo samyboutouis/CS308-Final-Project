7de9d2627196a18da39a5de1430d83a3697467ce
Bill Guo <wg78@duke.edu>
2021-04-20 02:04:52 -0400

    added frame perfect jump off pig

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:353a6cb51e49970028dd1d5786a9d35ddc56c0dfe0881db0dc839b1bf7cf0346
-size 2665
+oid sha256:b806108dea9df09fd77ef31895369b189c4e01a3d4f2f022e8653385917a87d9
+size 2726
