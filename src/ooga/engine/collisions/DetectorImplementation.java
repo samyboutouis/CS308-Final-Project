c901aea6cbf6dcad95e9c4edebd1ad511bbbfa52
Bill Guo <wg78@duke.edu>
2021-04-21 01:26:46 -0400

    changed collision tolerance

--- src/ooga/engine/collisions/DetectorImplementation.java
+++ src/ooga/engine/collisions/DetectorImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f5ece27b93dd532ce4b3cec738ef652efb9309919c9174f481260ed738c98de
+oid sha256:4a258c28520be8762cbaac3311044404a9ed221809d3b0af630ba59603fd3f01
 size 1461
