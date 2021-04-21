64fbb0c04a260bc3c04fd1256e59a4c831bfad58
Samy Boutouis <sb590@duke.edu>
2021-04-20 22:42:58 -0400

    Change imageview image

--- src/ooga/player/screens/ProfileScreen.java
+++ src/ooga/player/screens/ProfileScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb0ac1c7f0a750317fcb163e3b5acb4637b65c858aca7ad5458be90cf136bfa7
-size 3120
+oid sha256:5d48d6440e4437843c74c97e71c97e98500d814a5518560a5418a723d706f30a
+size 3123
