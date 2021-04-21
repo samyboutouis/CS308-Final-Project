4318b788461044eed605b744009d582da11fdea0
Samy Boutouis <sb590@duke.edu>
2021-04-21 01:35:30 -0400

    Add stylesheet combobox

--- src/ooga/Main.java
+++ src/ooga/Main.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:885815058e999d367b2332bf8a880d2ac74d3ba171a37479a02043f55099c5ad
-size 1510
+oid sha256:b28b98f0e7ce642fd5947be601329b5daf2300ca15c5fccf338ce1cafba1b1d4
+size 1509
--- src/ooga/player/screens/ProfileScreen.java
+++ src/ooga/player/screens/ProfileScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5c93f66e0942bcdf092094231e1b5dbed887d0b5884e7431f60a53261f0f20a
-size 3452
+oid sha256:12354d013134ee3a01e88718478819b6fdb6968d0ec69856ba233c2e38f44cd1
+size 4231
