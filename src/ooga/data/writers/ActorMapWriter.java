792a1601122f0409058e972da2b65f760c8cd302
Kevin Yang <kevin.yang@duke.edu>
2021-04-20 22:16:55 -0400

    Changed writer file structure

---
+++ src/ooga/data/writers/ActorMapWriter.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d77c1cf8231a13be1fb71a9896bfaec2434dfd7f8773acc01eebaba9b6376171
+size 61
---
+++ src/ooga/data/writers/ReflectiveParser.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70f8105e45949ca10b7455b14573ab8f1b8cb9339625f89926ff29ce2e56078e
+size 521
