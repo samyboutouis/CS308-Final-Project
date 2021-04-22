59ef316cc8ba04a34d4f3c311adbfbf9e0ea5ea1
Donghan Park <donghan.park@duke.edu>
2021-04-21 21:21:36 -0400

    added click tile to change image feature

--- src/ooga/player/screens/EditorGridTile.java
+++ src/ooga/player/screens/EditorGridTile.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2cce82fa1b935719b597c81a2e54545c05871c4fe5ffd2a423ef7a752a0ff40
-size 1047
+oid sha256:fa7cfc775da50c6bd2c84298486b90970b7314264883144adf6254965a83fc81
+size 1095
