9f890846e0f2def7112f4243adb854ca2ecd619e
Felix Jiang <felix.jiang@duke.edu>
2021-04-22 17:48:10 -0400

    comment purpose of next and snapshot for velocities

--- src/ooga/engine/actors/Actor.java
+++ src/ooga/engine/actors/Actor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d1dd6b800460fd13bff4481bdb54644b11e3c6ec444014fe70ad6822e4bbc11
-size 6833
+oid sha256:4ce1db0015ffb2baec9e3607657e4a15fddd20813973c0e535e763ca08b69458
+size 7400
