f4bd0316db4ef9a7534a311e66bd2c3fc134f27d
Felix Jiang <felix.jiang@duke.edu>
2021-04-22 18:45:19 -0400

    give description a key and run a test method

--- src/ooga/data/DataParser.java
+++ src/ooga/data/DataParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ac975274d5b7aa068752662d3ae00a3a407f59517ec650b48cb200746bbd35
-size 1439
+oid sha256:d173bbe9618dbadc5b3ec01132cda028dde71f4c55d56926df29a1341e5daa58
+size 1535
--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daadc730cd4e83e5620c667e63d1c8e45e23bc5009823f081e5d09c9b5490985
-size 2696
+oid sha256:76e67aeb3562d924b1cafbe77acb481dc7cf95641c3690cc2cd51466e26b6285
+size 2720
