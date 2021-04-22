816a5f5469338d2f8a6515bbcde4eff6f8b6e6b8
Felix Jiang <felix.jiang@duke.edu>
2021-04-22 17:28:26 -0400

    level parser does not need to know actor index

--- src/ooga/data/LevelParser.java
+++ src/ooga/data/LevelParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d008ab39f04629b9af23a046801c0c8fd68d121c5b358d985c66287c136904ea
-size 4261
+oid sha256:f8b2a5b01c52c653fdf1f3efd26fb5d973b15a0ca2d4addf201d5bb22e8d0de2
+size 4226
