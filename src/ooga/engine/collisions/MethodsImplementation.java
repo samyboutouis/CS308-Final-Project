14fea4634ea6ee5bc8bfe05c4f06e780f17cd8a4
Bill Guo <wg78@duke.edu>
2021-04-24 19:04:25 -0400

    added fire flower

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6627d13d3b21cfa2402250f24b283e122d7c69db60bb79cb91d143ce8e796e4
-size 4654
+oid sha256:f18f1fbfdfb7902133468e56e971bd76b1bb336b252ba59096a6974e3578286a
+size 4801
