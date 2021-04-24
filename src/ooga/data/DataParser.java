d1eac22099a9fb0709fc95368696bc9889410a8a
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 18:29:09 -0400

    data parser description value will also be needed

--- src/ooga/data/DataParser.java
+++ src/ooga/data/DataParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eaf8a9abfe38a60adca944f01c1b22574a79b07f0c0cb79353835d97813bccc
-size 1538
+oid sha256:3ad11633414d6974bab4904ce7adce1865318fd28d79c104750d8bd325a86f42
+size 1484
