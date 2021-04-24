ec42788222113b890dcba6f2a277714114eaf600
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 19:23:38 -0400

    lowercase name for query but uppercase for post. query filter for delete and find are more specific

--- web/test.js
+++ web/test.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dacd44e86cf3122e2cd4ede4cd51614d0a398ae56708815791e3fd26e67fb65c
-size 4333
+oid sha256:e12702e0932ec03337b0b667cca2e6604e9729ed5136107a7e7ba50573dedf76
+size 4391
