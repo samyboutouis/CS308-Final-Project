0e5dea1ea5e3d4f34cb801fc48907e201d1a9912
Donghan Park <donghan.park@duke.edu>
2021-04-24 19:37:23 -0400

    changed actor map to show file name

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a1f8a6cd27bc6efa715ce844bc8ede10b97196e165050040d8f58f89c10d2fd
-size 8425
+oid sha256:7970e9403f213a5c4f3a4e1e9ceb98345930bc163654aa0ceee189e3fde09f1a
+size 8654
