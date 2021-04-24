e1e55af9fe412845d023e2ef3e9ceea5840f076c
Kevin Yang <kevin.yang@duke.edu>
2021-04-24 16:35:54 -0400

    Added getters for movementmanager

--- src/ooga/engine/actions/MovementManager.java
+++ src/ooga/engine/actions/MovementManager.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41df4bb7d267f33b730ccd6c838b4623d4958efa8b4f5e75cb50238672417c21
-size 2091
+oid sha256:8eeca7be47dbb79144169c965eece9799f95927be4588028b3fa1fe6b4ec33cc
+size 2566
