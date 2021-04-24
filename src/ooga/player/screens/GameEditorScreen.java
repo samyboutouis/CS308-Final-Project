5ff4a36b4d63bbc60ef8ad8bbb26b696b036d2bb
Samy Boutouis <sb590@duke.edu>
2021-04-24 18:54:19 -0400

    Change styling for profile screen

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b9822b14b1bc94266d0887988e818e959cdcf60c81f3ef0281a64c78bb4154f
-size 2641
+oid sha256:8b2b3d0314bbdc879af0ceb958e6f71f97a1599d76907d7665b12fc40e87044d
+size 2640
--- src/ooga/player/screens/ProfileScreen.java
+++ src/ooga/player/screens/ProfileScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:825ae7b0132caadb8896e06ddf7684ac0ef0c929895868e26a04803ac544d3a3
-size 5015
+oid sha256:20bd28e06edd5085039875431cc3b5580531e17febe47a208c9290ede173e129
+size 5147
