c9485d1fa596b24b236938016427bf53cf3688bd
Kevin Yang <kevin.yang@duke.edu>
2021-04-24 16:37:47 -0400

    getter for PropertyList

--- src/ooga/engine/property/PropertyList.java
+++ src/ooga/engine/property/PropertyList.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3a3cfa27f28cb2963f39c17e66b80716fc713ef947d8c40f2d890b1217072ef
-size 1504
+oid sha256:628ab8bac8c5643cce033a90a2d0e291edca01be48d61d0167fb28b8b1bc0e69
+size 1728
