4307a752714fd250aeac098da4b5bafffeba991a
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 21:27:50 -0400

    write a directory for level files into a single json for web

--- src/ooga/data/PrimaryParser.java
+++ src/ooga/data/PrimaryParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87f5d78622a25e39957552e0616166c5e62ab389ab96b2ecd6c29f8d976e5301
-size 3765
+oid sha256:2c7accafa47a41f13c7f6817b39b5b58c9765070d450157d13c04b5b1b5083b0
+size 3763
