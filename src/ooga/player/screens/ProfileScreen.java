3ea5d4463f6cb7653d2e962b953f8536cd6dbc6d
Samy Boutouis <sb590@duke.edu>
2021-04-24 22:39:14 -0400

    Sign in screen constants

--- src/ooga/player/screens/ProfileScreen.java
+++ src/ooga/player/screens/ProfileScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20bd28e06edd5085039875431cc3b5580531e17febe47a208c9290ede173e129
-size 5147
+oid sha256:8ec21b402d49fa1fafa709b0aa374d2fb5fe908a1f6ba5c4685532dc1836e1da
+size 4870
--- src/ooga/player/screens/SignInScreen.java
+++ src/ooga/player/screens/SignInScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:014fbd3eab7c27dc6c9e7037d678e2a2ef6ce02dcf43163e63e5f756902e47ec
-size 4197
+oid sha256:27c1c39cc78326ccd2d8d49c990275bf6bc4388a8c5954eddb9068ae41baf0c6
+size 4486
