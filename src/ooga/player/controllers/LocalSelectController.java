76cb2558aade78c140def38e14d89c31e8247b08
Samy Boutouis <sb590@duke.edu>
2021-04-25 15:44:11 -0400

    Run multiple games at once and rename game select screen to local select screen

---
+++ src/ooga/player/controllers/LocalSelectController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc8f67131368a562553c6f75f339feb5ea28ba09d5b5671a19eddd05539f6680
+size 1669
--- src/ooga/player/screens/SplashScreen.java
+++ src/ooga/player/screens/SplashScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4f9178fabf3c90c6f8705ebf13ccf2e0ac083f0eb6f31490efdb0a330286260
-size 4078
+oid sha256:7b70ea553290c3ac1703e81ceccf74499344b9a5500024d07a8be7a269747962
+size 4080
