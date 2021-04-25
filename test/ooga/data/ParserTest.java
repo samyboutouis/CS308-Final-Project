fd8568b8cb2998059380b34b909a8cecb67bdc12
Felix Jiang <felix.jiang@duke.edu>
2021-04-25 17:25:43 -0400

    final run of tests should keep level1 in the webLevel directory

--- test/ooga/data/ParserTest.java
+++ test/ooga/data/ParserTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:972020cfdbc79c617e26084a6dbcc9667599045f082bbf9617301e6673abfa51
-size 3274
+oid sha256:de51ad061b6a1aac9b202e81816947841a4fae552cccc83332d9727a3e2706af
+size 3417
