e54c64f4e0ced5809fad504b066f61b9501e73c6
Donghan Park <donghan.park@duke.edu>
2021-04-25 00:13:17 -0400

    fix

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3334cf656a891a816458497373160902e5bf73fbf055ced6c1cf83c8ab57da35
-size 11255
+oid sha256:c3e5dbb41debcac950f23bbba07942c708bb4f9c23b49271090a10980d057acb
+size 10660
