45edba328220a7180add0c1be973b893cc1081f2
Samy Boutouis <sb590@duke.edu>
2021-04-25 19:56:24 -0400

    Descriptions for the web

--- src/ooga/player/screens/LocalSelectScreen.java
+++ src/ooga/player/screens/LocalSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1faad54bc1613b317d15a3bc1388b417d2c142a972e8059d00340d474f9ee21a
-size 2288
+oid sha256:93b829fb0366aba0db4c42adf9048287c1df307b045ce9482a274f8b90cc7495
+size 2540
