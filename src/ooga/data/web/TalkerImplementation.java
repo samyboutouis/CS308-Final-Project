32b440604fa60bc75ef4bc88fc1888e51872830b
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 23:04:47 -0400

    use real URL and return an array for all names and descriptions

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abbd027f3a4c0ca1bea0322a7dd2ceb20d549366dcb134e6ffa6510b93736202
-size 1787
+oid sha256:a1dd788ee014c106ca86fab5901c4367ad520da847df7e91850e0f7fb609ca64
+size 1838
