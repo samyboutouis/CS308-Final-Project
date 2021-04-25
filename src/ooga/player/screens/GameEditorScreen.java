f0ed818c4af5708f413fd2710aa3e2c4b330cfa1
Donghan Park <donghan.park@duke.edu>
2021-04-25 18:46:29 -0400

    updated collisions data file for editor & refactored editor controller

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:479f1fad8f937de1e9017fda5a94f94f5b050b37f68203ae4e6d3e84b08ae103
-size 6639
+oid sha256:0ef607026362bdeaa8cc9968650bc3626fea7ad8044a7ab5c30ed44afce79d1c
+size 6796
