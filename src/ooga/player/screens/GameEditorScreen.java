84a1d0a0f9f7df92701316afe94e70ce5ce5e8cf
Donghan Park <donghan.park@duke.edu>
2021-04-24 21:42:40 -0400

    initialized level writer

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ec5c9a082d741e14ff50dc50ea517db9687f660e6a8c95797e67ebf23fbcee1
-size 9415
+oid sha256:aae512756357d7afeb57ee9fb13ce926244012e5d7281ec905154476bf84076c
+size 9452
