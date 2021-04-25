6c322583ffa3f57888de132609de956fb48c887a
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 21:44:09 -0400

    allow a level to be uploaded to the web

--- src/ooga/data/PrimaryParser.java
+++ src/ooga/data/PrimaryParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c7accafa47a41f13c7f6817b39b5b58c9765070d450157d13c04b5b1b5083b0
-size 3763
+oid sha256:1860255a1366dbd5cdac6c63b03693264c8e7abc08d420ee9eb5d34399f625a5
+size 4198
