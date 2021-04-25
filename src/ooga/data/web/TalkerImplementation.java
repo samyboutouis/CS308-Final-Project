5fd82f98849bbfadd1ec1121f83815376fd742a1
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 22:33:38 -0400

    can upload data to web with code closes #59

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a80eff89236d4ac9a17c9156671378622948597fe7d6a2e30d848ec87d39d6e
-size 1723
+oid sha256:abbd027f3a4c0ca1bea0322a7dd2ceb20d549366dcb134e6ffa6510b93736202
+size 1787
