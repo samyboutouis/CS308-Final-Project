4d208a10730ae95e8a205610a6c252c343126f99
Felix Jiang <felix.jiang@duke.edu>
2021-04-25 17:31:44 -0400

    ensure tests work regardless of what ran before them

--- test/ooga/data/ParserTest.java
+++ test/ooga/data/ParserTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de51ad061b6a1aac9b202e81816947841a4fae552cccc83332d9727a3e2706af
-size 3417
+oid sha256:e1f59033285f6c5fafe48765ebd1f8e7155a2a7c2807b779d7375422cfbd85ae
+size 3613
