4a1bb8b0b2c2d37d67a7e588091dee2b941d8c77
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 21:26:48 -0400

    get actor json from JSONParser

--- src/ooga/data/JSONParser.java
+++ src/ooga/data/JSONParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62b2668439f594c3704709e79e98d62da61f0cb9512d20baef88b2c16419a85
-size 2496
+oid sha256:2a1e3ac7698cedf9cb4a43305497858391f6e0ab669ae28e43fc90bf03fb137d
+size 3027
