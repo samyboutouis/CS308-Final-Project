5b89d2555840146f50f2446b1e7bbf9f4bafdee9
Samy Boutouis <sb590@duke.edu>
2021-04-25 18:02:40 -0400

    Refactor constants in profile screen

--- src/ooga/player/screens/ProfileScreen.java
+++ src/ooga/player/screens/ProfileScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:763fcf1f54edf73c6acc00a4e6507b5700a99e53c482ce311fdececcd953a76e
-size 4871
+oid sha256:b6c8dc8ba88fcafb968b4906b62356c848b602798ffa95446a34fe023b2538df
+size 5689
