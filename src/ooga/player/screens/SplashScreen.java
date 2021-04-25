df662ff69e43e5d26f6fa7d53c14ab38dde1193b
Samy Boutouis <sb590@duke.edu>
2021-04-25 15:19:58 -0400

    Create upload web button

--- src/ooga/player/screens/SplashScreen.java
+++ src/ooga/player/screens/SplashScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8812a2a04edbc709199ac3f5a8f3f1b43824e49e8cd36c7f0aab00f3b165e6f4
-size 3446
+oid sha256:c4f9178fabf3c90c6f8705ebf13ccf2e0ac083f0eb6f31490efdb0a330286260
+size 4078
