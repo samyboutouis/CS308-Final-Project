41b45850a33bd473db38c57dbeb36cdac832d5ac
Samy Boutouis <sb590@duke.edu>
2021-04-24 21:01:05 -0400

    Complete sign-in process completed

--- src/ooga/player/screens/SignInScreen.java
+++ src/ooga/player/screens/SignInScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bcc4d2ae4eb62d8a8c13ab10c900565e9e5cd025b95ff3fce0ee3bc6ca1f04e
-size 3084
+oid sha256:014fbd3eab7c27dc6c9e7037d678e2a2ef6ce02dcf43163e63e5f756902e47ec
+size 4197
