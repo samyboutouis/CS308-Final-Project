5b8ae74a66fff5b538024f1d751c829893b01f50
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 23:16:28 -0400

    json object is in key response for get requests

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1dd788ee014c106ca86fab5901c4367ad520da847df7e91850e0f7fb609ca64
-size 1838
+oid sha256:a1a0ca1435f72cf4a553686324467bfbf1f3ecd7ac8f6cf5088788ee1323577b
+size 1864
