ff256c4618daeedaffb9f5e6efb5f985cf3599db
Donghan Park <donghan.park@duke.edu>
2021-04-24 21:38:33 -0400

    added directory chooser for level saving

--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32f5a542f41438a78cce9b8913dea1fbc90594222daff17cbd8e8166792083b9
-size 9156
+oid sha256:1ec5c9a082d741e14ff50dc50ea517db9687f660e6a8c95797e67ebf23fbcee1
+size 9415
