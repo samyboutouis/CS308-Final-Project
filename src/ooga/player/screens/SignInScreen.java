fa4678e7b401257d6bc53fb22b557a6db2d7f4e8
Samy Boutouis <sb590@duke.edu>
2021-04-24 20:30:06 -0400

    Implement sign-in verification

--- src/ooga/player/screens/SignInScreen.java
+++ src/ooga/player/screens/SignInScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22b3d0a24cec4ef86fa84140644a03fa562fec492fe1f4aeeb59ff5020d8b494
-size 1786
+oid sha256:7bcc4d2ae4eb62d8a8c13ab10c900565e9e5cd025b95ff3fce0ee3bc6ca1f04e
+size 3084
