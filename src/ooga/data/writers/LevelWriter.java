8ed386ba76678a37b5351b78bba63a4bb5e1d624
Donghan Park <donghan.park@duke.edu>
2021-04-25 01:10:36 -0400

    removed print statements

--- src/ooga/data/writers/LevelWriter.java
+++ src/ooga/data/writers/LevelWriter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21a995fe1b1039b56773286182f7aa5095fd9fb0f275fdba97df560cdae36a9d
-size 4588
+oid sha256:494b79b9908dce53167bd3af778d0aad2c1c50f842497cdeeddfde458c896665
+size 4449
