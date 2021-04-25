321b845c7f3265ae309203c8fbc0c2c683855428
Kevin Yang <kevin.yang@duke.edu>
2021-04-25 17:40:19 -0400

    Added addTrigger test

--- test/ooga/data/LevelTest.java
+++ test/ooga/data/LevelTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2c313d72970c71fb2c0726a58a5b4ff6151ab56ddc53b64502be6677b70cfe0
-size 3174
+oid sha256:77600bc0c88b2ce1aa234b4c90277575add3ab840633ec54b521dfa0579d880f
+size 4194
