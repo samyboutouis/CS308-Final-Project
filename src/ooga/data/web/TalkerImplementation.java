c513b443c52814f360afa3c1ef26df94c601144e
Samy Boutouis <sb590@duke.edu>
2021-04-25 00:57:10 -0400

    Finish web select screen

--- src/ooga/data/web/TalkerImplementation.java
+++ src/ooga/data/web/TalkerImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1004261b28a149456475695ec15cd4fbe0b2063a0c3c9f489bdd6a166bf87070
+oid sha256:a1dd788ee014c106ca86fab5901c4367ad520da847df7e91850e0f7fb609ca64
 size 1838
--- src/ooga/player/screens/WebSelectScreen.java
+++ src/ooga/player/screens/WebSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29d0f99c301326d92fde7a3c4288014dbe3c18ff5bdc765fec72b951dbf1b362
-size 2229
+oid sha256:c161b90e94a9a1c932d656796c64b624b8219783f28acee31544abaf09c4e879
+size 2364
