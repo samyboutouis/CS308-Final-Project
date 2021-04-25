76e7e6686f7c611c33cf45eaff3a30c547542cf3
Samy Boutouis <sb590@duke.edu>
2021-04-25 16:22:21 -0400

    Remove magic files

--- src/ooga/player/screens/SignInScreen.java
+++ src/ooga/player/screens/SignInScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27c1c39cc78326ccd2d8d49c990275bf6bc4388a8c5954eddb9068ae41baf0c6
-size 4486
+oid sha256:d876cf517cca26e498737ddc075f71a3894b1a062718b705777511038196c141
+size 5225
