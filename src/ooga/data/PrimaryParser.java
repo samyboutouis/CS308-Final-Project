468af9a5809bf72784a3f8c56dac1916dcb7bac4
Felix Jiang <felix.jiang@duke.edu>
2021-04-24 20:42:15 -0400

    notes for converting files into a single JSON

--- src/ooga/data/PrimaryParser.java
+++ src/ooga/data/PrimaryParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e3ca1988a45089891d19a29c972d61ddd9504eeb21ae3506869e38f58bb12fa
-size 3533
+oid sha256:87f5d78622a25e39957552e0616166c5e62ab389ab96b2ecd6c29f8d976e5301
+size 3765
