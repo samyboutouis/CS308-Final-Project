c2f269d99cb7df7d8060c400664502113d6f8fdd
Kevin Yang <kevin.yang@duke.edu>
2021-04-25 23:31:10 -0400

    Readded reflective keys

--- src/ooga/data/TriggerParser.java
+++ src/ooga/data/TriggerParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f1eb8cc89f1ab743a8bad2214edac34bec166d0788ac6d1b8b33fe978faa889
-size 4261
+oid sha256:87b52ac5519abb4b69203eaddd12fd131588dbf8138a564593d177dc976efdd3
+size 3893
