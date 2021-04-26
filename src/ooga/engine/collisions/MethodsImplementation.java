711df6f76f45be14f19d11c48c1eda4508362d90
Felix Jiang <felix.jiang@duke.edu>
2021-04-25 22:19:34 -0400

    kill enemies with invincible actor

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a41a81c704bae83d1f29ae35ed646c980ffb194bff22ffb93bc01773234cb4e
-size 6161
+oid sha256:67571d4a272ee2db544236c0f33d4bfc05866b79cd0b314b94b8d5b1c41465e7
+size 6231
