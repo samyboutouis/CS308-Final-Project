a99059c49433216333e6710c8e8d2155fb18878e
Donghan Park <donghan.park@duke.edu>
2021-04-26 00:21:21 -0400

    fixed editor tests

--- test/ooga/player/GameEditorTest.java
+++ test/ooga/player/GameEditorTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4352ec1b48c63019a1aa75cf1767755e257ff94acf3f7af8c551f101e26b4b67
-size 3062
+oid sha256:bd83fe5f819f69b1425cc0b72d104795649d707f589b4c4249050c4909bb567a
+size 3064
