a0138cc06c5e982c9167d63991cc61295c284267
Bill Guo <wg78@duke.edu>
2021-04-26 01:23:48 -0400

    added lives from data

--- src/ooga/engine/LevelImplementation.java
+++ src/ooga/engine/LevelImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc350d40f225b51a64e683a448427c839acb7eb642c5528bd9be913cfe55eb31
-size 8938
+oid sha256:fee313ec38ed43071a6462b4c774f4e7e3f551c181c997ce66861753d94949b3
+size 8967
--- test/ooga/engine/EngineTest.java
+++ test/ooga/engine/EngineTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d702776a9fd5e6034255afe3ec7fc781ef8effc763bb2c9eb0e0679e511b06a
-size 14407
+oid sha256:4001473099acd6379ff8ed57d94a879df78ed704c397f2f2a18cca9101e6439d
+size 14409
