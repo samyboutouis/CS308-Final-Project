aea989b38de64cbc93bf0d88d87cf4ec9a2969cc
Kevin Yang <kevin.yang@duke.edu>
2021-04-25 22:04:50 -0400

    Added tests for most actions

--- test/ooga/engine/EngineTest.java
+++ test/ooga/engine/EngineTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc900d52766a4a92cff50e807c024d71af5cb047bcb97e5285a8ad0d48c6d28a
-size 8127
+oid sha256:75acec20e5e7a33d39b2e27bb1c8249dfaf185bb20220b53e29ee7bd05656891
+size 10475
