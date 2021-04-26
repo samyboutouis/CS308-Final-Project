6356035df338910b703184f0912b6cedd8140c15
Donghan Park <donghan.park@duke.edu>
2021-04-25 20:12:49 -0400

    clicking cancel for filechoosers does not result in error

--- src/ooga/player/controllers/GameEditorController.java
+++ src/ooga/player/controllers/GameEditorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:484b73f421c27c76720571cc94fff9b43a6accd57136bcc86f43a93b436aa3fd
-size 5324
+oid sha256:106d35d6e0f969e3e18637ce2b9c374999ea60d45d7a5707c72d0b13ef659ac3
+size 5432
