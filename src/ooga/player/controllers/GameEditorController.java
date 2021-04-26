e4b5692cab61c6c7bfbb831ea034917b2bd11b2e
Donghan Park <donghan.park@duke.edu>
2021-04-25 20:09:56 -0400

    editor supports different level sizes

--- src/ooga/player/controllers/GameEditorController.java
+++ src/ooga/player/controllers/GameEditorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b9b06d9e0502fa75638a7511b9e9c94dcad04a58705f56f2f543187d91735dc
-size 5222
+oid sha256:484b73f421c27c76720571cc94fff9b43a6accd57136bcc86f43a93b436aa3fd
+size 5324
--- src/ooga/player/screens/GameEditorScreen.java
+++ src/ooga/player/screens/GameEditorScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ef607026362bdeaa8cc9968650bc3626fea7ad8044a7ab5c30ed44afce79d1c
-size 6796
+oid sha256:0b55547e16cb23af5f2a6abcfaa402e7f4e636f28692536dc5cf4c1206bcc18e
+size 6928
