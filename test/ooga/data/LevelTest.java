53b582e59f90832f6862d12e1b66795212daf5bb
Kevin Yang <kevin.yang@duke.edu>
2021-04-26 01:00:00 -0400

    Increased coverage for LevelTest

--- test/ooga/data/LevelTest.java
+++ test/ooga/data/LevelTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e33ce2e6902d02e29af69b7ebf1bacc54570f7ad34876dd1806e91952c798cb
-size 6893
+oid sha256:e4dbfa8a0fc8358d473f0591cd5a6d4d6fd18810ad66968a8577d7196057fd42
+size 6875
