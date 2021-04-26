d5b8b3bff09c3022dfc4aaf1b62ee2d735f14909
Samy Boutouis <sb590@duke.edu>
2021-04-25 23:32:43 -0400

    Upload to web

--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139bb6c52e86ca65f0aefd6427abab4fca803da06b8ee0f6cbbaeaf80765311e
-size 1930
+oid sha256:5de59133b1dedd04d2d20d69221528de9c62b1c423e676965762a4bcb34bb6e3
+size 1711
--- src/ooga/player/screens/SplashScreen.java
+++ src/ooga/player/screens/SplashScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:150d2484da8cbaa9fd9de3044d7dacb1e037d1fc541ad065b62dbac8b0b0df11
-size 2940
+oid sha256:4a77bc65763170b1f7ac4a01d8003644b50f1c0befbebd70909c83f7503b0d83
+size 2617
