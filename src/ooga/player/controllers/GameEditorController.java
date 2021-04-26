a853c73591fc0f2780287c71df2bfdc445132340
Donghan Park <donghan.park@duke.edu>
2021-04-25 21:50:32 -0400

    fixed hardcoded filenames

--- src/ooga/player/controllers/GameEditorController.java
+++ src/ooga/player/controllers/GameEditorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a63c15a196247b887547af9b9d21bea82850133a94c3a534c859db08405cd28
-size 5639
+oid sha256:c59301466b5f69efb83e63d89e7b1f4b33d7e161c2fac53847619e870c7e64bd
+size 5785
