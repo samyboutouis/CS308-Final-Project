c1d9e1f0a8142855f7d144aaec568f8b6a7a1066
Felix Jiang <felix.jiang@duke.edu>
2021-04-26 02:09:01 -0400

    remove hardcoded score values

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ab40128056da6c873147fa25f8fa348544aafaa1af0e8b2969a86cbfa2794f
-size 8059
+oid sha256:61df262fc1cdc2b70cd8697d716e25d10105758363a45fcca2efc2bfc8e9f97c
+size 7876
