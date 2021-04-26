fb2b115f439d2969c118b382e72edb493c8634d7
Felix Jiang <felix.jiang@duke.edu>
2021-04-25 22:45:26 -0400

    grid parser doesn't need to catch file not found

--- src/ooga/data/GridParser.java
+++ src/ooga/data/GridParser.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63f88ec9371a7822b97546abd6bfa827e1d846660167f8e7af98881b60e06c5e
-size 873
+oid sha256:e3c484d91b8f9f21e2cbe32ad29b31b9209a80fda9af6294593adb556eacfe72
+size 904
