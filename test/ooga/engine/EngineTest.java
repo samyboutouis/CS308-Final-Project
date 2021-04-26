edac20c808a521e182f36adb7b331274d87e8d68
Kevin Yang <kevin.yang@duke.edu>
2021-04-26 01:17:45 -0400

    Added getDimensions test

--- test/ooga/engine/EngineTest.java
+++ test/ooga/engine/EngineTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d702776a9fd5e6034255afe3ec7fc781ef8effc763bb2c9eb0e0679e511b06a
-size 14407
+oid sha256:25966e8a1beba9f6fde481972828ad9119f106e36aaaed81d0dafedcba5204a5
+size 14639
