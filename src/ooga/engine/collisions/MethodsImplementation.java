705df5c34d70096eaa7f8d6d9c7436d04ef6fd82
Felix Jiang <felix.jiang@duke.edu>
2021-04-26 01:47:10 -0400

    new methods for changing sprite based on  powerup

--- src/ooga/engine/collisions/MethodsImplementation.java
+++ src/ooga/engine/collisions/MethodsImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b9faf40025ef8533405fd70827e292f7870dc495d997ad59078a3be5ddfbb83
-size 8197
+oid sha256:fffb20e0c2aed79af22229240ccc9cf454d5eda004e458156c4453d6c8bdd0ff
+size 8456
