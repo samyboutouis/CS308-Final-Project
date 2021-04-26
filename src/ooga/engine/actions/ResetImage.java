a9b5ffc148554b33a363d160647da043db893c1c
Felix Jiang <felix.jiang@duke.edu>
2021-04-26 01:41:20 -0400

    notify front end that image changed

--- src/ooga/engine/actions/ResetImage.java
+++ src/ooga/engine/actions/ResetImage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53596b3caca708c371d98ef5ddd5fc04ab5087ce956032a5974892f13139696e
-size 593
+oid sha256:4eb654dc21c793079549b2f162c41a17795de0d51e2616a43d899c3c4e1edb5a
+size 650
