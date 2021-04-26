a82f905d7274c0b8bf201cc75e50b7c00540c76b
Donghan Park <donghan.park@duke.edu>
2021-04-25 22:55:40 -0400

    refactored editor & select screens/controller

--- src/ooga/player/controllers/GameSelectController.java
+++ src/ooga/player/controllers/GameSelectController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5710fc8eaa5e0b786cf238d8f06d6b27e790c681f978c26521eb2e510bf72db5
-size 1153
+oid sha256:d98f98ecfe154591f309fa60a855098a4bccd72952fc659b771d19ccc190a227
+size 1289
