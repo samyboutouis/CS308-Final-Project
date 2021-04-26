428dba704c6fa63b24f23e7e29c3fc7122f24ba7
Felix Jiang <felix.jiang@duke.edu>
2021-04-26 02:05:57 -0400

    actor has a kill score

--- src/ooga/engine/actors/Actor.java
+++ src/ooga/engine/actors/Actor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cafc88e3e3af1b40a80a99b31f753a742138f7fe2d362b2da8343ce65aa6a957
-size 7524
+oid sha256:6ef99547980fd5af9b16bc2eb68b32fdb24620517615086946e359efd64a4273
+size 7734
