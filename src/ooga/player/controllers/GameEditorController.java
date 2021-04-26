2672f795a9b69d07cd492df1dd3c66ff34bdf934
Donghan Park <donghan.park@duke.edu>
2021-04-25 22:23:20 -0400

    temp fix

--- src/ooga/player/controllers/GameEditorController.java
+++ src/ooga/player/controllers/GameEditorController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2518324d36e91a388efd525ec9d698bd8d186e17afa7c859f1b760200b04acdb
-size 5797
+oid sha256:854975704d6981284d277720e50a153e59d7f1cc04b51b30bfcc49528bec9cce
+size 5803
--- src/ooga/player/screens/GameSelectScreen.java
+++ src/ooga/player/screens/GameSelectScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dbfa2910a723d57da3f9713efac550f20eb2f4c37a39a031390037e2cac4af4
-size 1701
+oid sha256:376d0e53d65e507bbfda1167e2e04f968823caaff0a58bdbdd5f453328983f1f
+size 1961
