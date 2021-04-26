4cb73ff51a54d21e69ee80ae056fa3ce8a9bfbca
Samy Boutouis <sb590@duke.edu>
2021-04-25 20:12:13 -0400

    Remove play again button

--- src/ooga/player/screens/EndScreen.java
+++ src/ooga/player/screens/EndScreen.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e26e629eafff6132835ffe70e603bf37240e9f41a3a6007811a75ee3a9fce954
-size 1342
+oid sha256:2da516d84b03102412e77f8b564c1c79bbd40b30182cc0fbd74ae129f86a3efc
+size 1216
