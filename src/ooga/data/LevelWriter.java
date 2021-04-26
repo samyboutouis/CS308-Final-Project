22699e46d35545afb8393e22c792586b29544ea4
Kevin Yang <kevin.yang@duke.edu>
2021-04-26 01:01:40 -0400

    Changed exception structure for coverage

--- src/ooga/data/LevelWriter.java
+++ src/ooga/data/LevelWriter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc7e2d3f461d5b77c4c3a2d673522f5af0261ab76c6df576f872674a7e1b5cf9
-size 4686
+oid sha256:38831d899249b7691a5add741312800e6d3cf8445bb67fcd960e6e08c8a9b776
+size 4956
