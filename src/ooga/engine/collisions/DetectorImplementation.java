b78aea494ba3612fcc1b01cf98f362db08b163e6
Felix Jiang <felix.jiang@duke.edu>
2021-04-28 21:53:58 -0400

    collision manager detector methods

--- src/ooga/engine/collisions/DetectorImplementation.java
+++ src/ooga/engine/collisions/DetectorImplementation.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e05294fbdbd339124e87713e44dc7ba8623ba829c183c00166c1af20a44d6018
-size 1492
+oid sha256:b4cdaa5cafe72308ca56865b818f2b2dbb30118a2bb1a72635b2b8a93a73508a
+size 2585
